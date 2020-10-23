49a5b61e70dcbe5723d904d909675068941950ec
Reto Barmettler <reto@fambarm.ch>
2020-10-23 19:13:03 +0200

    its makes waves!

--- android/src/main/kotlin/tv/mta/flutter_playout/audio/AudioServiceBinder.java
+++ android/src/main/kotlin/tv/mta/flutter_playout/audio/AudioServiceBinder.java
@@ -473,6 +473,8 @@ public class AudioServiceBinder
                     PlayerNotificationUtil.getActionIntent(context, KeyEvent.KEYCODE_MEDIA_PLAY));
         }
 
+        notificationBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText("Your Long Text here"))
+
         NotificationManager notificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
 
