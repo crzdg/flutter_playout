d62497722f24c9946308ec72fa6819cd7d536695
Reto Barmettler <reto@fambarm.ch>
2020-11-01 21:55:00 +0100

    save those lines

--- ios/Classes/SwiftFlutterPlayoutPlugin.swift
+++ ios/Classes/SwiftFlutterPlayoutPlugin.swift
@@ -2,14 +2,8 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterPlayoutPlugin: NSObject, FlutterPlugin {
-    
   public static func register(with registrar: FlutterPluginRegistrar) {
-    
     /* register audio player */
     AudioPlayer.register(with: registrar)
-
-    // TODO: video is not needed anymore
-    /* register video player */
-    VideoPlayerFactory.register(with: registrar)
   }
 }
